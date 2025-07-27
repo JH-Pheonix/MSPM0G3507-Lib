c4135ce9819f920dc14ec168d74fa07ea6ec14ab
ZeroHzzzz <3190381602@qq.com>
2025-07-27 18:04:31 +0800

    feat: 重构积分模块，支持三轴角度积分并更新接口

--- project/code/algo/Integral/Integral.c
+++ project/code/algo/Integral/Integral.c
@@ -1,4 +1,4 @@
-#include "YawIntegral.h"
+#include "Integral.h"
 // #include "control.h"  // 暂时注释掉，需要实现control相关函数
 
 // 临时实现restrictValueF函数
@@ -11,17 +11,33 @@ static Integral_info s_integral;
 
 void integral_init(float dt)
 {
+    s_integral.roll = 0.0f;
+    s_integral.pitch = 0.0f;
     s_integral.yaw = 0.0f;
     s_integral.dt = dt;
 }
 
 void integral_update(imu_data_t *data)
 {
-
-    s_integral.yaw += data->gyro_z;
+    // 对三个轴的角速度进行积分
+    s_integral.roll += data->gyro_x * s_integral.dt;
+    s_integral.pitch += data->gyro_y * s_integral.dt;
+    s_integral.yaw += data->gyro_z * s_integral.dt;
 
     // 限制积分值的范围，防止溢出或累积过大的误差
+    restrictValueF(&s_integral.roll, 1000.0f, -1000.0f);
+    restrictValueF(&s_integral.pitch, 1000.0f, -1000.0f);
     restrictValueF(&s_integral.yaw, 1000.0f, -1000.0f);
+}
+
+float integral_get_roll(void)
+{
+    return s_integral.roll;
+}
+
+float integral_get_pitch(void)
+{
+    return s_integral.pitch;
 }
 
 float integral_get_yaw(void)
@@ -31,5 +47,7 @@ float integral_get_yaw(void)
 
 void integral_reset(void)
 {
+    s_integral.roll = 0.0f;
+    s_integral.pitch = 0.0f;
     s_integral.yaw = 0.0f;
 }
