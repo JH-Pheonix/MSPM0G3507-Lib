082ae21fbbea71f7cae89e51892d07ba67d761c2
ZeroHzzzz <3190381602@qq.com>
2025-07-29 16:25:15 +0800

    feat: 添加云台坐标转换功能及相关数据结构

---
+++ project/code/common/common.h
@@ -0,0 +1,24 @@
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+#include "zf_common_headfile.h"
+
+
+typedef struct 
+{
+    float x;
+    float y;
+} point_2d_f_t;
+
+typedef struct 
+{
+    int32 x; // 俯仰角
+    int32 y;   // 偏航角
+} point_2d_i32_t;
+
+typedef struct {
+    float pan;
+    float tilt;
+} gimbal_angle_2d_t;
+
+#endif
