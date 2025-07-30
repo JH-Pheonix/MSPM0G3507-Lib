83d6b920c9ce15d404fff8f6f0b7ac9d2077b7c3
ZeroHzzzz <3190381602@qq.com>
2025-07-30 15:07:22 +0800

    feat: 添加系统初始化函数

--- project/code/system/init/init.c
+++ project/code/system/init/init.c
@@ -1,7 +1,15 @@
 #include "init.h"
 #include "imu.h"
+#include "key.h"
+#include "grey_tracking.h"
+#include "menu.h"
+#include "lcd.h"
 
 void system_init(void)
 {
-    imu_init(IMU_DEVICE_660RA); 
+    lcd_init();
+    imu_init(IMU_DEVICE_660RA);
+    key_init_rewrite(KEY_NUM);
+    grey_tracking_init(GREY_MID);
+    MainMenu_Set();
 }
