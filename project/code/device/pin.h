01aa78adea51416cbe4b3413e7a06557b9f00726
ZeroHzzzz <3190381602@qq.com>
2025-07-30 17:17:01 +0800

    feat: 修改外设引脚

--- project/code/device/pin.h
+++ project/code/device/pin.h
@@ -6,8 +6,8 @@
 #define ENCODER_SCLK_PIN SPI1_SCK_B23
 #define ENCODER_MOSI_PIN SPI1_MOSI_B22
 #define ENCODER_MISO_PIN SPI1_MISO_B21
-#define ENCODER_CNT 4
-#define ENCODER_CS_PIN_LIST { B17, B18, B26, B27 }
+#define ENCODER_CNT 2
+#define ENCODER_CS_PIN_LIST { B26, B27 }
 
 // 增量式编码器AB相引脚配置
 #define ENCODER_1_PIN A0, A1
@@ -16,12 +16,12 @@
 #define ENCODER_4_PIN A6, A7
 
 // ADIS pin
-#define ADIS_SPI                         (SPI_0           )
-#define ADIS_CS_PIN                      (B20             )
+#define ADIS_SPI                         (SPI_1           )
+#define ADIS_CS_PIN                      (B19             )
 #define ADIS_RST_PIN                     (B10             )
-#define ADIS_MOSI                        (SPI0_MOSI_B17   )
-#define ADIS_MISO                        (SPI0_MISO_B19   )
-#define ADIS_SCK                         (SPI0_SCK_B18    )
+#define ADIS_MOSI                        (SPI1_MOSI_B22   )
+#define ADIS_MISO                        (SPI1_MISO_B21   )
+#define ADIS_SCK                         (SPI1_SCK_B23    )
 #define ADIS_SPEED                       (2 * 1000 * 1000 )
 
 // tft180 pin
@@ -34,12 +34,12 @@
 #define LCD_BL_PIN                       (A13             ) // 背光引脚
 
 // key pin
-//                C    , A    , D    , B    , CENTER
+//                A    , C    , B    , D    , CENTER
 #define MKEY_NAME KEY_U, KEY_D, KEY_L, KEY_R, KEY_B
-#define MKEY_LIST A0, A10, A12, A15, A13
+#define MKEY_LIST B0, A30, B1, A31, A29
 
 // grey tracking pin
 #define GREY_TRACKING_NAME GREY_LEFT_SIDE, GREY_LEFT, GREY_MID, GREY_RIGHT, GREY_RIGHT_SIDE
-#define GREY_TRACKING_LIST A0, A1, A2, A3, A4
+#define GREY_TRACKING_LIST B17, B18, A21, A20, A23
 
 #endif
